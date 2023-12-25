4366c687ca892eb28d3873e4c3a3a15101594486
Interrupt <cuddigan@gmail.com>
2023-12-25 01:05:01 -0800

    Cleaning up build file

--- build.zig
+++ build.zig
@@ -1,132 +1,54 @@
 const std = @import("std");
-
-// const bx = @import("build_bx.zig");
-// const bimg = @import("build_bimg.zig");
-// const bgfx = @import("build_bgfx.zig");
-// const sc = @import("build_shader_compiler.zig");
 const sokol = @import("3rdparty/sokol-zig/build.zig");
 
-// ZigLua lib
-// const ziglua = @import("libs/ziglua/build.zig");
-
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "brass-emulator",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+        .name = "delve-framework",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
+    // Add Ziglua module
     const ziglua = b.dependency("ziglua", .{
         .target = target,
         .optimize = optimize,
     });
 
-    // Add modules
     exe.addModule("ziglua", ziglua.module("ziglua"));
     exe.linkLibrary(ziglua.artifact("lua"));
 
+    // Include Sokol from submodule
     const sokol_build = sokol.buildSokol(b, target, optimize, .{}, "3rdparty/sokol-zig/");
     exe.linkLibrary(sokol_build);
-
-    // Link some platforms
-    // if (target.isDarwin()){
-    //     // Add SDL2, include path may vary
-    //     exe.linkSystemLibrary("sdl2");
-    //     exe.linkFramework("Foundation");
-    //     exe.linkFramework("CoreFoundation");
-    //     exe.linkFramework("Cocoa");
-    //     exe.linkFramework("QuartzCore");
-    //     exe.linkFramework("OpenGL");
-    //     exe.linkFramework("IOKit");
-    //     exe.linkFramework("Metal");
-    // }
-    // else if (target.isWindows()) {
-    //     exe.addIncludePath(.{ .path = "3rdparty/sdl2/windows/include"});
-    //     exe.addLibraryPath(.{ .path = "3rdparty/sdl2/windows/win64"});
-    //     exe.linkSystemLibrary("sdl2");
-    //     exe.linkSystemLibrary("opengl32");
-    //     exe.linkSystemLibrary("gdi32");
-    //     exe.linkSystemLibrary("winmm");
-    //     exe.linkSystemLibrary("setupapi");
-    //     exe.linkSystemLibrary("ole32");
-    //     exe.linkSystemLibrary("oleaut32");
-    //     exe.linkSystemLibrary("imm32");
-    //     exe.linkSystemLibrary("version");
-    // }
-
-    exe.linkSystemLibrary("c");
-    exe.linkSystemLibrary("c++");
-
-    // Link the bgfx libs
-    // bx.link(exe);
-    // bimg.link(exe);
-    // bgfx.link(exe);
-
     exe.addAnonymousModule("sokol", .{ .source_file = .{ .path = "3rdparty/sokol-zig/src/sokol/sokol.zig" } });
 
     // Add sdb_image single header library for image file format support
     exe.addCSourceFile(.{ .file = .{ .cwd_relative = "libs/stb_image-2.28/stb_image_impl.c"}, .flags = &[_][]const u8{"-std=c99"}});
     exe.addIncludePath(.{ .path = "libs/stb_image-2.28"});
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     const install_exe = b.addInstallArtifact(exe, .{});
     b.getInstallStep().dependOn(&install_exe.step);
 
-    // This *creates* a RunStep in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    //const run_cmd = exe.run();
-
     const run_cmd = b.addRunArtifact(exe);
-    //b.getInstallStep().dependOn(&run_exe.step);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing.
     const exe_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
