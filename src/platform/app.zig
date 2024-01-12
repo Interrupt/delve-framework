5b7aac6468021f5931e434c0d958f129cd492239
Interrupt <cuddigan@gmail.com>
2024-01-11 20:09:53 -0800

    Splitting out the app platform from its backend implementation

--- src/platform/app.zig
+++ src/platform/app.zig
@@ -1,35 +1,34 @@
-const std = @import("std");
 const app = @import("../app.zig");
 const debug = @import("../debug.zig");
 const gfx = @import("graphics.zig");
-const input = @import("input.zig");
 const modules = @import("../modules.zig");
+const sokol_app_backend = @import("backends/sokol/app.zig");
 
-const sokol = @import("sokol");
-const slog = sokol.log;
-const sg = sokol.gfx;
-const sapp = sokol.app;
-const sgapp = sokol.app_gfx_glue;
+// Actual app backend, implementation could be switched out here
+const AppBackend = sokol_app_backend.App;
 
 pub fn init() !void {
     debug.log("App starting", .{});
+
+    AppBackend.init(.{
+        .on_init_fn = on_init,
+        .on_cleanup_fn = on_cleanup,
+        .on_frame_fn = on_frame,
+    });
 }
+
 
 pub fn deinit() void {
     debug.log("App stopping", .{});
+    AppBackend.deinit();
 }
 
-export fn sokol_init() void {
-    debug.log("Sokol app backend initializing", .{});
+pub fn startMainLoop(config: app.AppConfig) void {
+    AppBackend.startMainLoop(config);
+}
 
-    sg.setup(.{
-        .context = sgapp.context(),
-        .logger = .{ .func = slog.func },
-        .buffer_pool_size = 256, // default is 128
-    });
-
-    debug.log("Sokol setup backend: {}\n", .{sg.queryBackend()});
-
+fn on_init() void {
+    // Start graphics first
     gfx.init() catch {
         debug.log("Fatal error initializing graphics backend!\n", .{});
         return;
@@ -41,23 +40,22 @@ export fn sokol_init() void {
         return;
     };
 
-    // initialize modules first
+    // initialize modules finally
     modules.initModules();
 
     // then kick everything off!
     modules.startModules();
 }
 
-export fn sokol_cleanup() void {
+fn on_cleanup() void {
     modules.stopModules();
     modules.cleanupModules();
     app.stopSubsystems();
     gfx.deinit();
-    sg.shutdown();
 }
 
 var tick: u64 = 0;
-export fn sokol_frame() void {
+fn on_frame() void {
     tick += 1;
 
     // tick first
@@ -70,40 +68,4 @@ export fn sokol_frame() void {
 
     // tell modules this frame is done
     modules.postDrawModules();
-}
-
-export fn sokol_input(event: ?*const sapp.Event) void {
-    const ev = event.?;
-    if(ev.type == .MOUSE_DOWN) {
-        input.onMouseDown(@intFromEnum(ev.mouse_button));
-    } else if(ev.type == .MOUSE_UP) {
-        input.onMouseUp(@intFromEnum(ev.mouse_button));
-    } else if (ev.type == .MOUSE_MOVE) {
-        input.onMouseMoved(ev.mouse_x, ev.mouse_y);
-    } else if(ev.type == .KEY_DOWN) {
-        input.onKeyDown(@intFromEnum(ev.key_code));
-    } else if(ev.type == .KEY_UP) {
-        input.onKeyUp(@intFromEnum(ev.key_code));
-    } else if(ev.type == .CHAR) {
-        input.onKeyChar(ev.char_code);
-    }
-}
-
-pub fn startMainLoop(config: app.AppConfig) void {
-    sapp.run(.{
-        .init_cb = sokol_init,
-        .frame_cb = sokol_frame,
-        .cleanup_cb = sokol_cleanup,
-        .event_cb = sokol_input,
-        .width = config.width,
-        .height = config.height,
-        .icon = .{
-            .sokol_default = true,
-        },
-        .window_title = config.title,
-        .logger = .{
-            .func = slog.func,
-        },
-        .win32_console_attach = true,
-    });
 }
---
+++ src/platform/backends/sokol/app.zig
@@ -0,0 +1,103 @@
+
+const std = @import("std");
+const main_app = @import("../../../app.zig");
+const debug = @import("../../../debug.zig");
+// const gfx = @import("graphics.zig");
+const input = @import("../../input.zig");
+// const modules = @import("../modules.zig");
+
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sgapp = sokol.app_gfx_glue;
+
+pub const SokolAppConfig = struct {
+    on_init_fn: *const fn () void,
+    on_frame_fn: *const fn () void,
+    on_cleanup_fn: *const fn () void,
+    // maybe an on_event fn?
+};
+
+// keep a static version of the app around
+var app: App = undefined;
+
+pub const App = struct {
+    on_init_fn: *const fn () void,
+    on_frame_fn: *const fn () void,
+    on_cleanup_fn: *const fn () void,
+
+    pub fn init(cfg: SokolAppConfig) void {
+        debug.log("Creating Sokol App backend", .{});
+
+        app = App {
+            .on_init_fn = cfg.on_init_fn,
+            .on_frame_fn = cfg.on_frame_fn,
+            .on_cleanup_fn = cfg.on_cleanup_fn,
+        };
+    }
+
+    pub fn deinit() void {
+        debug.log("Sokol App Backend stopping", .{});
+    }
+
+    export fn sokol_init() void {
+        debug.log("Sokol app context initializing", .{});
+
+        sg.setup(.{
+            .context = sgapp.context(),
+            .logger = .{ .func = slog.func },
+            .buffer_pool_size = 256, // default is 128
+        });
+
+        debug.log("Sokol setup backend: {}\n", .{sg.queryBackend()});
+
+        // call the callback that will tell everything else to start up
+        app.on_init_fn();
+    }
+
+    export fn sokol_cleanup() void {
+        app.on_cleanup_fn();
+        sg.shutdown();
+    }
+
+    export fn sokol_frame() void {
+        app.on_frame_fn();
+    }
+
+    export fn sokol_input(event: ?*const sapp.Event) void {
+        const ev = event.?;
+        if(ev.type == .MOUSE_DOWN) {
+            input.onMouseDown(@intFromEnum(ev.mouse_button));
+        } else if(ev.type == .MOUSE_UP) {
+            input.onMouseUp(@intFromEnum(ev.mouse_button));
+        } else if (ev.type == .MOUSE_MOVE) {
+            input.onMouseMoved(ev.mouse_x, ev.mouse_y);
+        } else if(ev.type == .KEY_DOWN) {
+            input.onKeyDown(@intFromEnum(ev.key_code));
+        } else if(ev.type == .KEY_UP) {
+            input.onKeyUp(@intFromEnum(ev.key_code));
+        } else if(ev.type == .CHAR) {
+            input.onKeyChar(ev.char_code);
+        }
+    }
+
+    pub fn startMainLoop(config: main_app.AppConfig) void {
+        sapp.run(.{
+            .init_cb = sokol_init,
+            .frame_cb = sokol_frame,
+            .cleanup_cb = sokol_cleanup,
+            .event_cb = sokol_input,
+            .width = config.width,
+            .height = config.height,
+            .icon = .{
+                .sokol_default = true,
+            },
+            .window_title = config.title,
+            .logger = .{
+                .func = slog.func,
+            },
+            .win32_console_attach = true,
+        });
+    }
+};
