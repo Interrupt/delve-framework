7a0f559074c8e52a96cb7454d3427183bc7c139f
Interrupt <cuddigan@gmail.com>
2024-01-27 23:53:28 -0800

    Building as a static library

--- build.zig
+++ build.zig
@@ -29,14 +29,25 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    buildExample(b, "audio", target, optimize, delve_module);
-    buildExample(b, "sprites", target, optimize, delve_module);
-    buildExample(b, "clear", target, optimize, delve_module);
-    buildExample(b, "debugdraw", target, optimize, delve_module);
-    buildExample(b, "forest", target, optimize, delve_module);
-    buildExample(b, "lua", target, optimize, delve_module);
-    buildExample(b, "meshes", target, optimize, delve_module);
-    buildExample(b, "stresstest", target, optimize, delve_module);
+    // Delve library artifact
+    const lib_opts = .{
+        .name = "delve",
+        .target = target,
+        .optimize = optimize,
+    };
+
+    const lib = b.addStaticLibrary(lib_opts);
+    linkDelveFramework(b, lib, target, optimize);
+    b.installArtifact(lib);
+
+    buildExample(b, "audio", target, optimize, delve_module, lib);
+    buildExample(b, "sprites", target, optimize, delve_module, lib);
+    buildExample(b, "clear", target, optimize, delve_module, lib);
+    buildExample(b, "debugdraw", target, optimize, delve_module, lib);
+    buildExample(b, "forest", target, optimize, delve_module, lib);
+    buildExample(b, "lua", target, optimize, delve_module, lib);
+    buildExample(b, "meshes", target, optimize, delve_module, lib);
+    buildExample(b, "stresstest", target, optimize, delve_module, lib);
 
     const exe_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/framework/delve.zig" },
@@ -52,12 +63,6 @@ pub fn linkDelveFramework(b: *std.Build, step: *std.Build.CompileStep, target: a
     step.addCSourceFile(.{ .file = .{ .path = "libs/stb_image-2.28/stb_image_impl.c" }, .flags = &[_][]const u8{"-std=c99"} });
     step.addIncludePath(.{ .path = "libs/stb_image-2.28" });
 
-    const ziglua = b.dependency("ziglua", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    step.linkLibrary(ziglua.artifact("lua"));
-
     const sokol_build = sokol.buildSokol(b, target, optimize, .{}, "3rdparty/sokol-zig/");
     step.linkLibrary(sokol_build);
 
@@ -66,9 +71,23 @@ pub fn linkDelveFramework(b: *std.Build, step: *std.Build.CompileStep, target: a
 
     const zmesh_pkg = zmesh.package(b, target, optimize, .{});
     zmesh_pkg.link(step);
+
+    const ziglua = b.dependency("ziglua", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    step.linkLibrary(ziglua.artifact("lua"));
+
+    step.installHeader("libs/stb_image-2.28/stb_image.h", "stb_image.h");
+
+    // why do we have to install these? Shouldn't the ziglua lib have them already?
+    step.installHeader("libs/ziglua/lib/lua-5.4/src/lua.h", "lua/lua.h");
+    step.installHeader("libs/ziglua/lib/lua-5.4/src/luaconf.h", "lua/luaconf.h");
+    step.installHeader("libs/ziglua/lib/lua-5.4/src/lualib.h", "lua/lualib.h");
+    step.installHeader("libs/ziglua/lib/lua-5.4/src/lauxlib.h", "lua/lauxlib.h");
 }
 
-pub fn buildExample(b: *std.Build, comptime name: []const u8, target: anytype, optimize: anytype, delve_module: *std.Build.Module) void {
+pub fn buildExample(b: *std.Build, comptime name: []const u8, target: anytype, optimize: anytype, delve_module: *std.Build.Module, lib: *std.Build.CompileStep) void {
     const src_main = "src/examples/" ++ name ++ ".zig";
 
     const example = b.addExecutable(.{
@@ -79,7 +98,7 @@ pub fn buildExample(b: *std.Build, comptime name: []const u8, target: anytype, o
     });
 
     example.addModule("delve", delve_module);
-    linkDelveFramework(b, example, target, optimize);
+    example.linkLibrary(lib);
 
     b.installArtifact(example);
     const run = b.addRunArtifact(example);
