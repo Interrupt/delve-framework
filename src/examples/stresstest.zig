96aee5f11ea18cc19017fef72ddd561b02941c35
Interrupt <cuddigan@gmail.com>
2024-01-29 00:19:13 -0800

    Stresstest includes Lua module again

--- src/examples/stresstest.zig
+++ src/examples/stresstest.zig
@@ -8,6 +8,8 @@ const meshes_example = @import("meshes.zig");
 const sprites_example = @import("sprites.zig");
 const forest_example = @import("forest.zig");
 
+const lua_module = delve.module.lua_simple;
+
 // This example loads a bunch of the examples to stress test everything!
 // Also a good example of how the module system can be used to compose apps
 
@@ -16,6 +18,7 @@ pub fn main() !void {
     try audio_example.registerModule();
     try debugdraw_example.registerModule();
     try meshes_example.registerModule();
+    try lua_module.registerModule();
     try sprites_example.registerModule();
     // try forest_example.registerModule();
 
