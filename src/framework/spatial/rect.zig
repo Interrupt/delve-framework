d2addcf1270dcc864c0dab44dc2c600df16ff0d0
Interrupt <cuddigan@gmail.com>
2024-02-06 13:57:06 -0800

    Rectangles have an origin now

--- src/framework/spatial/rect.zig
+++ src/framework/spatial/rect.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const math = @import("../math.zig");
+const debug = @import("../debug.zig");
 
 const Vec2 = math.Vec2;
 
@@ -10,8 +11,7 @@ pub const Rect = struct {
     height: f32,
 
     // positioning
-    x_centered: bool = false,
-    y_centered: bool = false,
+    origin: ?Vec2 = null,
 
     /// Creates a new rectangle, where pos is the bottom-left
     pub fn new(pos: Vec2, size: Vec2) Rect {
@@ -30,8 +30,7 @@ pub const Rect = struct {
             .y = pos.y - (size.y * 0.5),
             .width = size.x,
             .height = size.y,
-            .x_centered = true,
-            .y_centered = true,
+            .origin = pos,
         };
     }
 
@@ -45,6 +44,15 @@ pub const Rect = struct {
         };
     }
 
+    /// Adds an origin to a rectangle. Default is bottom left.
+    pub fn withOrigin(self: *const Rect, origin: Vec2) Rect {
+        var rect = self.*;
+        rect.origin = origin;
+
+        var diff = if (self.origin != null) self.origin.?.sub(origin) else origin.scale(-1);
+        return rect.translate(diff);
+    }
+
     /// Creates a new rectangle, where the center is zero
     pub fn fromSizeCentered(size: Vec2) Rect {
         return Rect{
@@ -52,32 +60,16 @@ pub const Rect = struct {
             .y = -size.y * 0.5,
             .width = size.x,
             .height = size.y,
-            .x_centered = true,
-            .y_centered = true,
+            .origin = Vec2.zero(),
         };
     }
 
-    /// Gets the rect position, honoring the centering
+    /// Gets the rect position, which will be the origin if given or the bottom left if not
     pub fn getPosition(self: *const Rect) Vec2 {
-        var pos = Vec2.new(self.x, self.y);
+        if (self.origin != null)
+            return self.origin.?;
 
-        if (self.x_centered)
-            pos.x += self.width * 0.5;
-        if (self.y_centered)
-            pos.y += self.height * 0.5;
-
-        return pos;
-    }
-
-    /// Sets the rect position, honoring the centering
-    pub fn setPosition(self: *Rect, pos: Vec2) void {
-        self.x = pos.x;
-        self.y = pos.y;
-
-        if (self.x_centered)
-            self.x -= self.width * 0.5;
-        if (self.y_centered)
-            self.y -= self.height * 0.5;
+        return Vec2.new(self.x, self.y);
     }
 
     pub fn getSize(self: *const Rect) Vec2 {
@@ -122,57 +114,41 @@ pub const Rect = struct {
 
     /// Returns a centered version of this rectangle, where the original position will be now the center
     pub fn centered(self: *const Rect) Rect {
-        var pos = Vec2.new(self.x, self.y);
-
-        if (!self.x_centered)
-            pos.x -= self.width * 0.5;
-        if (!self.y_centered)
-            pos.y -= self.height * 0.5;
-
-        var rect = Rect.new(pos, self.getSize());
-        rect.x_centered = true;
-        rect.y_centered = true;
-        return rect;
+        var center = self.getCenter();
+        return self.withOrigin(center);
     }
 
     /// Returns a box where only the X axis is centered
     pub fn centeredX(self: *const Rect) Rect {
-        if (self.x_centered)
-            return self.*;
+        var center = self.getCenter();
+        var pos = self.getPosition();
+        center.y = pos.y;
 
-        var pos = Vec2.new(self.x, self.y);
-        pos.x -= self.width * 0.5;
-        var rect = Rect.new(pos, self.getSize());
-        rect.x_centered = true;
-        return rect;
+        return self.withOrigin(center);
     }
 
     /// Returns a box where only the Y axis is centered
     pub fn centeredY(self: *const Rect) Rect {
-        if (self.y_centered)
-            return self.*;
+        var center = self.getCenter();
+        var pos = self.getPosition();
+        center.x = pos.x;
 
-        var pos = Vec2.new(self.x, self.y);
-        pos.y -= self.width * 0.5;
-        var rect = Rect.new(pos, self.getSize());
-        rect.y_centered = true;
-        return rect;
+        return self.withOrigin(center);
     }
 
     pub fn scale(self: *const Rect, scale_by: f32) Rect {
-        var pos = Vec2.new(self.x, self.y);
-        var size = self.getSize().scale(scale_by);
+        var rect = self.*;
 
-        var rect = Rect.new(pos, size);
+        rect.width *= scale_by;
+        rect.height *= scale_by;
+        rect.x *= scale_by;
+        rect.y *= scale_by;
 
-        // If we were a centered rect, do the offset as well
-        if (self.x_centered)
-            rect.x -= rect.width * 0.5;
-        if (self.y_centered)
-            rect.y -= rect.height * 0.5;
-
-        rect.x_centered = self.x_centered;
-        rect.y_centered = self.y_centered;
+        // cancel out translation - but origin stays the same
+        if (self.origin) |origin| {
+            rect.x += origin.x - (origin.x * scale_by);
+            rect.y += origin.y - (origin.y * scale_by);
+        }
 
         return rect;
     }
@@ -182,6 +158,13 @@ pub const Rect = struct {
         var rect = self.*;
         rect.x += move_by.x;
         rect.y += move_by.y;
+
+        // origin also may need to move
+        if (rect.origin != null) {
+            rect.origin.?.x += move_by.x;
+            rect.origin.?.y += move_by.y;
+        }
+
         return rect;
     }
 };
