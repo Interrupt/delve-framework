2dfd5c41f2754f5f1f47292d035441be58e3f662
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-02-13 00:41:51 +0200

    create submodules 1

--- 3rdparty/sokol-zig/src/sokol/c/sokol_audio.c
+++
@@ -1,6 +0,0 @@
-#if defined(IMPL)
-#define SOKOL_AUDIO_IMPL
-#endif
-#include "sokol_defines.h"
-#include "sokol_audio.h"
-
--- 3rdparty/sokol-zig/src/sokol/c/sokol_log.c
+++
@@ -1,5 +0,0 @@
-#if defined(IMPL)
-#define SOKOL_LOG_IMPL
-#endif
-#include "sokol_defines.h"
-#include "sokol_log.h"
