8f6a5b7c64698f7235a687d9f16533e876812ed6
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2024-02-13 00:41:51 +0200

    create submodules 1

--- 3rdparty/sokol-zig/src/sokol/c/sokol_gfx.c
+++
@@ -1,5 +0,0 @@
-#if defined(IMPL)
-#define SOKOL_GFX_IMPL
-#endif
-#include "sokol_defines.h"
-#include "sokol_gfx.h"
--- 3rdparty/sokol-zig/src/sokol/c/sokol_time.c
+++
@@ -1,5 +0,0 @@
-#if defined(IMPL)
-#define SOKOL_TIME_IMPL
-#endif
-#include "sokol_defines.h"
-#include "sokol_time.h"
