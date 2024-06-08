631a7efb4b131e70bb4116212fcc63056fe65fd8
Interrupt <cuddigan@gmail.com>
2024-06-08 11:04:10 -0700

    Animating the clear example

--- src/examples/clear.zig
+++ src/examples/clear.zig
@@ -3,7 +3,11 @@ const app = delve.app;
 const std = @import("std");
 const builtin = @import("builtin");
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
 // This example does nothing but open a blank window!
+
+var time: f32 = 0.0;
 
 pub fn main() !void {
     std.debug.print("starting clear example\n", .{});
@@ -11,6 +15,7 @@ pub fn main() !void {
     const clear_module = delve.modules.Module{
         .name = "clear_example",
         .init_fn = on_init,
+        .tick_fn = on_tick,
     };
 
     // Pick the allocator to use depending on platform
@@ -19,7 +24,6 @@ pub fn main() !void {
         // See https://github.com/ziglang/zig/issues/19072
         try delve.init(std.heap.c_allocator);
     } else {
-        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
         try delve.init(gpa.allocator());
     }
 
@@ -30,5 +34,17 @@ pub fn main() !void {
 
 pub fn on_init() !void {
     std.debug.print("clear module on_init()\n", .{});
+
     delve.platform.graphics.setClearColor(delve.colors.examples_bg_light);
+}
+
+pub fn on_tick(delta: f32) void {
+    time += delta;
+
+    var bg_color = delve.colors.examples_bg_light;
+    bg_color.r = (@sin(time * 2.0) + 1.0) * 0.5;
+    bg_color.g = (@sin(time * 2.3) + 1.0) * 0.5;
+    bg_color.b = (@sin(time * 2.5) + 1.0) * 0.5;
+
+    delve.platform.graphics.setClearColor(bg_color);
 }
