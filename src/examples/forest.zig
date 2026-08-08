9a876b5837ca0fee130710cfc5d3e3e746ac5296
Interrupt <cuddigan@gmail.com>
2026-08-08 00:18:58 -0700

    Fixing the forest and framepacing examples

--- src/examples/forest.zig
+++ src/examples/forest.zig
@@ -42,16 +42,16 @@ pub const module = modules.Module{
 // This is an example of using the sprite batcher to draw a forest!
 // shows off: sprite batches, texture regions, billboarding, cameras
 
-pub fn main() !void {
+pub fn main(init: std.process.Init) !void {
     // Pick the allocator to use depending on platform
     const builtin = @import("builtin");
     if (builtin.os.tag == .wasi or builtin.os.tag == .emscripten) {
         // Web builds hack: use the C allocator to avoid OOM errors
         // See https://github.com/ziglang/zig/issues/19072
-        try delve.init(std.heap.c_allocator);
+        try delve.init(init, std.heap.c_allocator);
     } else {
         // Using the default allocator will let us detect memory leaks
-        try delve.init(delve.mem.createDefaultAllocator());
+        try delve.init(init, delve.mem.createDefaultAllocator());
     }
 
     try registerModule();
